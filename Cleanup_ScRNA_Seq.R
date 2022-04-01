dea10ec35e8d07e02fabb18055f2134ff950edce
balayev1 <64073191+balayev1@users.noreply.github.com>
2022-04-01 14:50:37 -0400

    Update Cleanup_ScRNA_Seq.R

--- Cleanup_ScRNA_Seq.R
+++ Cleanup_ScRNA_Seq.R
@@ -1,4 +1,4 @@
-##aggregating data after STARSolo
+#aggregating data after STARSolo
 
 
 #  saved as aggregate_STARSolo.R
@@ -58,7 +58,7 @@ nBarcodes <- 30000
 ## this the parent directory for the sample outputs
 topLevel.dir <- "/vast/palmer/home.grace/aj597/scratch60/Ed_cutadap/sample_out"
 ### we will work in analysis folder
-setwd("/vast/palmer/home.grace/ab3478/scratch60")
+setwd("/vast/palmer/home.grace/aj597/scratch60")
 
 sample.paths <- list.files(topLevel.dir, full.names=TRUE)
 sample.names <- basename(sample.paths)
@@ -100,9 +100,9 @@ collectSTARsolo <- function(sample.path, nBarcodes){
     percSpliced.g[is.infinite(percSpliced.g)] <- 100
 ################## get the scVelo spliced/unspliced estimate of spliced fraction
     velocyto.path <- file.path(sample.path, "Solo.out", "Velocyto","raw")
-    spliced.mtx <- readr::read_delim(file.path(velocyto.path, "spliced.mtx"), col_names = FALSE,  delim = ' ', skip = 3)
-    unspliced.mtx <- readr::read_delim(file.path(velocyto.path, "unspliced.mtx"), col_names = FALSE,  delim = ' ', skip = 3)
-    ambiguous.mtx <- readr::read_delim(file.path(velocyto.path, "ambiguous.mtx"), col_names = FALSE,  delim = ' ', skip = 3)
+    spliced.mtx <- readr::read_delim(file.path(velocyto.path, "spliced.mtx"), col_names = FALSE, show_col_types=FALSE, delim = ' ', skip = 3)
+    unspliced.mtx <- readr::read_delim(file.path(velocyto.path, "unspliced.mtx"), col_names = FALSE, show_col_types=FALSE, delim = ' ', skip = 3)
+    ambiguous.mtx <- readr::read_delim(file.path(velocyto.path, "ambiguous.mtx"), col_names = FALSE, show_col_types=FALSE, delim = ' ', skip = 3)
     # velo.t <- readr::read_delim(file.path(velocyto.path, "matrix.mtx"), col_names = FALSE, delim = ' ', skip = 3)
     v.temp.barcodes <- read.table(file.path(velocyto.path, "barcodes.tsv"), sep="\t", header=FALSE)[,1]
 #### split the three levels of information into separate matrices
@@ -134,7 +134,7 @@ collectSTARsolo <- function(sample.path, nBarcodes){
 #### collect the remaining metadata values of interest
     gf.nUMI <- Matrix::colSums(gf.counts)
     gf.nGenes <- Matrix::colSums(gf.counts > 0)
-    mito.genes <- grep("^MT-", rownames(gf.counts), value=TRUE)
+    mito.genes <- grep("^mt-", rownames(gf.counts), value=TRUE)
     percMito <- 100*(Matrix::colSums(gf.counts[mito.genes,])/gf.nUMI)
     percMito[is.nan(percMito)] <- 0
     percMito[is.infinite(percMito)] <- 100
@@ -166,7 +166,7 @@ library.keep<- c("OF", "F_HOX", "M_HOX", "YF", "YM", "OM", "F_NOX", "M_NOX")
 sample.names= sample.names[sample.names %in% library.keep]
 sample.paths=sample.paths[basename(sample.paths) %in% library.keep]
 
-for (i in 1:length(sample.paths)) { 
+for (i in 1:length(sample.paths)) {
   output <- collectSTARsolo(sample.paths[i], nBarcodes = 30000)
   list.counts[[i]] <- output$counts
   list.metadata[[i]] <- output$metadata
@@ -182,6 +182,24 @@ dim(metadata.matrix)
 # [1] 244066     10
 sub.ed <- CreateSeuratObject(counts=count.matrix, meta.data=metadata.matrix, names.delim="__")
 
+####### Add batch column
+batch1<-c("F_HOX", "F_NOX", "M_HOX","M_NOX")
+batch2<-c("OF" ,"OM" ,"YF" ,"YM")
+batch.list=c(rep(NA, dim(metadata.matrix)[1]))
+for (i in c(batch1,batch2)) {
+  if (i %in% batch1){
+    positions <- which(sub.ed@meta.data$orig.ident == i)
+    batch.list[positions] <- "batch1"
+  }
+  if (i %in% batch2) {
+    positions <- which(sub.ed@meta.data$orig.ident == i)
+    batch.list[positions] <- "batch2"
+  }
+}
+
+sub.ed@meta.data$batch <- batch.list
+
+
 
 #################
 ########## Make diagnostic plots
@@ -190,13 +208,13 @@ sub.ed@meta.data$log10nUMI <- log10(sub.ed@meta.data$nUMI)
 
 ########## Scatter plots
 ###### nUMI vs percSpliced
-png("ScatterPlot.nUMI.percSpliced.ED.png, res=200, unit="in", height=8, width=11)
-ggplot(sub.ed@meta.data, aes(x=percSpliced.v, y=log10nUMI)) + 
-    geom_point(aes(color=orig.ident)) +
-    xlab("Percent Spliced") + 
+png("ScatterPlot.nUMI.percSpliced.ED.png", res=200, unit="in", height=8, width=11)
+ggplot(sub.ed@meta.data, aes(x=percSpliced.v, y=log10nUMI)) +
+    geom_point(aes(color=batch)) +
+    xlab("Percent Spliced") +
     ylab("nUMI") +
-    scale_fill_manual(values = c("F_HOX"="red", "F_NOX"="brown", "M_HOX"="yellow", "M_NOX"="green", "OF"="lightblue",
-    "OM"="darkblue", "YF"="violet", "YM"="pink")) +
+    scale_fill_manual(values = c("batch1"="yellow", "batch2"="red")) +
+    scale_x_continuous(breaks=c(0,10,20,30,40,50,60,70,80,90,100)) +
     # scale_color_viridis(discrete = TRUE, option = "C")+
     # scale_fill_viridis(discrete = TRUE) +
     theme(panel.background = element_rect(fill = 'black'),
@@ -206,13 +224,15 @@ dev.off()
 
 
 ###### nUMI vs percUnspliced
-png("ScatterPlot.nUMI.percUnspliced.ED.png, res=200, unit="in", height=8, width=11)
-ggplot(sub.ed@meta.data, aes(x=percUnspliced.v, y=log10nUMI)) + 
-    geom_point(aes(color=orig.ident)) +
-    xlab("Percent Intronic") + 
+
+png("ScatterPlot.nUMI.percUnspliced.ED.png", res=200, unit="in", height=8, width=11)
+ggplot(sub.ed@meta.data, aes(x=percUnspliced.v, y=log10nUMI)) +
+    geom_point(aes(color=batch)) +
+    xlab("Percent Intronic") +
     ylab("nUMI") +
-    scale_fill_manual(values = c("F_HOX"="red", "F_NOX"="brown", "M_HOX"="yellow", "M_NOX"="green", "OF"="lightblue",
-    "OM"="darkblue", "YF"="violet", "YM"="pink")) +
+    scale_fill_manual(values = c("batch1"="yellow", "batch2"="red")) +
+    scale_x_continuous(breaks=c(0,10,20,30,40,50,60,70,80,90,100)) +
+    geom_vline(xintercept =7,color="white") +
     # scale_color_viridis(discrete = TRUE, option = "C")+
     # scale_fill_viridis(discrete = TRUE) +
     theme(panel.background = element_rect(fill = 'black'),
@@ -222,19 +242,66 @@ dev.off()
 
 
 ###### nUMI vs Mitochondrial
-ggplot(sub.ed@meta.data, aes(x=percMito, y=log10nUMI)) + 
-    geom_point(aes(color=orig.ident)) +
-    xlab("Percent Mitochondrial") + 
+png("ScatterPlot.nUMI.percMito.ED.png", res=200, unit="in", height=8, width=11)
+ggplot(sub.ed@meta.data, aes(x=percMito, y=log10nUMI)) +
+    geom_point(aes(color=batch)) +
+    xlab("Percent Mitochondrial") +
     ylab("nUMI") +
-    scale_fill_manual(values = c("F_HOX"="red", "F_NOX"="brown", "M_HOX"="yellow", "M_NOX"="green", "OF"="lightblue",
-    "OM"="darkblue", "YF"="violet", "YM"="pink")) +
+    scale_fill_manual(values = c("batch1"="yellow", "batch2"="red")) +
     # scale_color_viridis(discrete = TRUE, option = "D")+
     # scale_fill_viridis(discrete = TRUE) +
     theme(panel.background = element_rect(fill = 'black'),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank())
+dev.off()
+
+#####nUMIvs genes
+png("ScatterPlot.nUMI.gene.ED.png", res=200, unit="in", height=8, width=11)
+ggplot(sub.ed@meta.data, aes(x=nGene , y=log10nUMI)) +
+    geom_point(aes(color=batch)) +
+    xlab("Gene") +
+    ylab("nUMI") +
+    scale_fill_manual(values = c("batch1"="yellow", "batch2"="red")) +
+    #scale_x_continuous(breaks=c(0,10,20,30,40,50,60,70,80,90,100)) +
+    geom_vline(xintercept =300,color="white") +
+    # scale_color_viridis(discrete = TRUE, option = "C")+
+    #scale_fill_viridis(discrete = TRUE) +
+    theme(panel.background = element_rect(fill = 'black'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank())
+dev.off()
 
 
+#####Charaterize data unsliced<8%
+##Metadata add 1 if for barcodes with >8%
+sub.ed@meta.data$statut_unsp <- c(rep(NA,dim(metadata.matrix)[1]))
+sub.ed@meta.data[which( sub.ed@meta.data$percUnspliced.v <= 8),"statut_unsp"] = "0"
+sub.ed@meta.data[which( sub.ed@meta.data$percUnspliced.v > 8),"statut_unsp"] = "1"
+##Make the figure
+png("ScatterPlot.nUMI.gene.unsp.ED.png", res=200, unit="in", height=8, width=11)
+ggplot(sub.ed@meta.data, aes(x=nGene , y=log10nUMI)) +
+    geom_point(aes(color=statut_unsp)) +
+    xlab("Gene") +
+    ylab("nUMI") +
+    scale_fill_manual(values = c("0"="yellow", "1"="red")) +
+    #scale_x_continuous(breaks=c(0,10,20,30,40,50,60,70,80,90,100)) +
+    geom_vline(xintercept =300,color="white") +
+    # scale_color_viridis(discrete = TRUE, option = "C")+
+    #scale_fill_viridis(discrete = TRUE) +
+    theme(panel.background = element_rect(fill = 'black'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank())
+dev.off()
 
-
-
+png("ScatterPlot.nUMI.percMito.unsp.ED.png", res=200, unit="in", height=8, width=11)
+ggplot(sub.ed@meta.data, aes(x=percMito, y=log10nUMI)) +
+    geom_point(aes(color=statut_unsp)) +
+    xlab("Percent Mitochondrial") +
+    ylab("nUMI") +
+    scale_fill_manual(values = c("0"="yellow", "1"="red")) +
+    # scale_color_viridis(discrete = TRUE, option = "D")+
+    # scale_fill_viridis(discrete = TRUE) +
+    theme(panel.background = element_rect(fill = 'black'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank())
+dev.off()
